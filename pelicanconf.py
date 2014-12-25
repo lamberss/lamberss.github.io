5a3e9c2c5c54be5ee5b7205c19bb8ba3f0075d75
Steven E. Lamberson, Jr <steven.lamberson@gmail.com>
2014-12-24 23:11:08 -0600

    Switch to Chunk theme and remove .html from URLs.

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,16 @@ SITENAME = 'Gaming, Computing, and Physics'
 SITEURL = ''
 
 PATH = 'content'
-THEME = '/home/lamberss/.pelican-themes/pelican-bootstrap3'
+THEME = '/home/lamberss/.pelican-themes/chunk'
+
+# CHUNK theme inputs
+DEFAULT_DATE_FORMAT = ('%b %d %Y')
+SITESUBTITLE = 'or whatever else I might feel like talking about'
+#FOOTER_TEXT = 'Replace pelican credit'
+DISPLAY_CATEGORIES_ON_MENU = True
+SINGLE_AUTHOR = True
+#MINT = True
+#GOOGLE_ANALYTICS = 'Put your google code here'
 
 TIMEZONE = 'America/Chicago'
 
@@ -30,6 +39,19 @@ AUTHOR_FEED_RSS = None
 SOCIAL = (('Github', 'https://github.com/lamberss'),)
 
 DEFAULT_PAGINATION = 10
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}/index.html'
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+DRAFT_URL = 'drafts/{slug}/'
+DRAFT_SAVE_AS = 'drafts/{slug}/index.html'
+PAGE_URL = 'pages/{slug}'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
